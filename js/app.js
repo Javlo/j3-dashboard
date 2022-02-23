f85a6494a9b30e0b3d5c05822a5a54b9e4be4adb
Aleyana <annesophie.elsayyed.13560@stu.he-ferrer.eu>
2022-02-23 14:07:13 +0100

    txt into json into data

--- js/app.js
+++ js/app.js
@@ -4,48 +4,6 @@
       element.classList.toggle("dark-mode");
    }
 
-
-  //  chart.js
-
-
-  // const labels = [
-  //   'January',
-  //   'February',
-  //   'March',
-  //   'April',
-  //   'May',
-  //   'June',
-  //   'July',
-  //   'August',
-  //   'September',
-  //   'October',
-  //   'November',
-  //   'December'
-  // ];
-
-  // const data = {
-  //   labels: labels,
-  //   datasets: [{
-  //     label: 'Pic de revenus',
-  //     // backgroundColor: 'rgb(255, 99, 132)',
-  //     borderColor: 'rgb(255, 99, 132)',
-  //     data: [0, 10, 5, 2, 20, 30, 45, 20, 12, 30, 50, 100],
-  //   }]
-  // };
-
-  // const config = {
-  //   type: 'line',
-  //   data: data,
-  //   options: {}
-  // };
-
-  // const myChart = new Chart(
-  //   document.getElementById('myChart'),
-  //   config
-  // );
-
-  // bootstrap tables
-
   // Vue.js
   let app = Vue.createApp({
     data() {
@@ -89,3 +47,12 @@
     }
   })
   app.mount('#app')
+
+  fetch('inscriptions-paiements.txt')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+    console.log(data);
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
