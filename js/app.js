178c777696114abb6f85ca2121169ed182080d27
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2022-02-23 18:18:19 +0100

    demo array

--- js/app.js
+++ js/app.js
@@ -1,58 +1,66 @@
-    // nightmode
-    function myFunction() {
-      var element = document.body;
-      element.classList.toggle("dark-mode");
-   }
+// nightmode
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
-  // Vue.js
-  let app = Vue.createApp({
-    data() {
-      return {
-        message: 'Votre dashboard',
-          data:'',
-          subs_subtitle:'nouveaux inscrits',
-          visitors_subtitle: 'visiteurs uniques',
-          money_subtitle:'revenus totaux',
-          latest_subs_title: 'Dernières inscriptions',
-          latest_mails:'Derniers mails',
-          paiement_curve: 'Courbe des paiements',
-          latest_internships: 'Derniers stages/activités ajouté(e)s'
+function parseDate(dateStr) {
+  return new Date(parseInt(dateStr.substring(0,4)), parseInt(dateStr.substring(5,7))+1, parseInt(dateStr.substring(8,10)));
+}
 
-      }
-    }, // data
+// Vue.js
+let app = Vue.createApp({
+  data() {
+    return {
+      message: 'Votre dashboard',
+      data: '',
+      subs_subtitle: 'nouveaux inscrits',
+      visitors_subtitle: 'visiteurs uniques',
+      money_subtitle: 'revenus totaux',
+      latest_subs_title: 'Dernières inscriptions',
+      latest_mails: 'Derniers mails',
+      paiement_curve: 'Courbe des paiements',
+      latest_internships: 'Derniers stages/activités ajouté(e)s'
 
-    methods: {
-        show(message) {
-         this.data = message
-      }
-    }, //methods
-    
-    components:{
-    },
-  }) // create app
+    }
+  }, // data
 
-  app.component('line-chart', {
-    extends: VueChartJs.Line,
-    mounted () {
-      this.renderChart({
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 39, 10, 40, 39, 80, 40]
-          }
-        ]
-      }, {responsive: true, maintainAspectRatio: false})
+  methods: {
+    show(message) {
+      this.data = message
+    }
+  }, //methods
+
+  components: {
+  },
+}) // create app
+
+app.component('line-chart', {
+  extends: VueChartJs.Line,
+  mounted() {
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 39, 10, 40, 39, 80, 40]
+        }
+      ]
+    }, { responsive: true, maintainAspectRatio: false })
+  }
+})
+app.mount('#app')
+
+fetch('inscriptions-paiements.txt')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("#data = "+data.length);
+    for (i=0; i<data.length; i++) {
+      var registration = data[i];
+      console.log("activityDateStart = "+parseDate(registration.activityDateStart));
     }
   })
-  app.mount('#app')
-
-  fetch('inscriptions-paiements.txt')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-    console.log(data);
-    })
-    .catch((error) => console.error("FETCH ERROR:", error));
+  .catch((error) => console.error("FETCH ERROR:", error));
